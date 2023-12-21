294607473dffe76a98bee88f5eeedc267141e106
irfanalikhoso42 <154511146+irfanalikhoso42@users.noreply.github.com>
2023-12-21 15:08:16 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 import { watchFile, unwatchFile } from 'fs'
-import chalk from 'chalk'
+import chalk from 'آلسی'
 import { fileURLToPath } from 'url'
 import fs from 'fs'
 import fetch from 'node-fetch'
